0d7a4ee61a1e1ab9375b94b691651c02af39335c
tharunShiv <tharun.rocky14@gmail.com>
2018-03-16 22:29:54 +0530

    PSO modal is automatically created, perfect job until now, CO, PSO completely done with save and modal

--- app/Http/Controllers/Dsce1Controller.php
+++ app/Http/Controllers/Dsce1Controller.php
@@ -13,7 +13,7 @@ class Dsce1Controller extends Controller
         $title = $request->input('title');
         $dsce = Dsce1::where('title', $title)->get();
         //when we use get(), it is a collection, so youve to iterate over it or use zero index
-        return view('pages.viewpso1')->with('subjects', $dsce); 
+        return view('pages.viewpso1')->with('subjects', $dsce);
     }
 
     /**
@@ -65,7 +65,7 @@ class Dsce1Controller extends Controller
         }
         else {
             echo 'ERROR-STORE METHOD ELSE BLOCK - Contact Tharun';
-        } 
+        }
     }
 
     /**
@@ -102,39 +102,108 @@ class Dsce1Controller extends Controller
         //
         $dsce = Dsce1::find($id);
         //echo $dsce;
-        //echo $request->input('title'); 
+        //echo $request->input('title');
+        $a = array(0,0,0,0,0,0);
+        $b = array(0,0,0,0,0,0);
+        $c = array(0,0,0,0,0,0);
+
         $dsce->title = $request->input('title');
-        $dsce->zero1 = $request->input('zero1');
-        $dsce->zero2 = $request->input('zero2');
-        $dsce->zero3 = $request->input('zero3');
-       
-        $dsce->one1 = $request->input('one1');
-        $dsce->one2 = $request->input('one2');
-        $dsce->one3 = $request->input('one3');
-       
-        $dsce->two1 = $request->input('two1');
-        $dsce->two2 = $request->input('two2');
-        $dsce->two3 = $request->input('two3');
-       
-        $dsce->three1 = $request->input('three1');
-        $dsce->three2 = $request->input('three2');
-        $dsce->three3 = $request->input('three3');
-       
-        $dsce->four1 = $request->input('four1');
-        $dsce->four2 = $request->input('four2');
-        $dsce->four3 = $request->input('four3');
-        
-        $dsce->five1 = $request->input('five1');
-        $dsce->five2 = $request->input('five2');
-        $dsce->five3 = $request->input('five3');
-        
-        $dsce->m1 = $request->input('m1');
-        $dsce->m2 = $request->input('m2');
-        $dsce->m3 = $request->input('m3');
-       
-        
+        $a[0] = $dsce->zero1 = $request->input('zero1');
+        $b[0] = $dsce->zero2 = $request->input('zero2');
+        $c[0] = $dsce->zero3 = $request->input('zero3');
+
+        $a[1] = $dsce->one1 = $request->input('one1');
+        $b[1] = $dsce->one2 = $request->input('one2');
+        $c[1] = $dsce->one3 = $request->input('one3');
+
+        $a[2] = $dsce->two1 = $request->input('two1');
+        $b[2] = $dsce->two2 = $request->input('two2');
+        $c[2] = $dsce->two3 = $request->input('two3');
+
+        $a[3] = $dsce->three1 = $request->input('three1');
+        $b[3] = $dsce->three2 = $request->input('three2');
+        $c[3] = $dsce->three3 = $request->input('three3');
+
+        $a[4] = $dsce->four1 = $request->input('four1');
+        $b[4] = $dsce->four2 = $request->input('four2');
+        $c[4] = $dsce->four3 = $request->input('four3');
+
+        $a[5] = $dsce->five1 = $request->input('five1');
+        $b[5] = $dsce->five2 = $request->input('five2');
+        $c[5] = $dsce->five3 = $request->input('five3');
+
+
+        // Finding Max Rep of array a
+        $countA = array(0,0,0,0);
+        for($var=0 ; $var<6; $var++){
+            echo "  ",$a[$var];
+        }
+        for($var=0 ; $var<6; $var++){
+            $temp = $a[$var];
+            $countA[$temp] = $countA[$temp] + 1;
+        }
+        for($var=0 ; $var<4; $var++){
+            echo "  ",$countA[$var];
+        }
+        $max = $countA[0];
+        $indA = 0;  //The index of the element max repeted
+        for($var=1; $var<4; $var++){
+            if($countA[$var]>=$max){
+                $max = $countA[$var];
+                $indA = $var;
+            }
+        }
+
+        // Finding Max Rep of array b
+        $countB = array(0,0,0,0);
+        for($var=0 ; $var<6; $var++){
+            echo "  ",$b[$var];
+        }
+        for($var=0 ; $var<6; $var++){
+            $temp = $b[$var];
+            $countB[$temp] = $countB[$temp] + 1;
+        }
+        for($var=0 ; $var<4; $var++){
+            echo "  ",$countB[$var];
+        }
+        $max = $countB[0];
+        $indB = 0;  //The index of the element max repeted
+        for($var=1; $var<4; $var++){
+            if($countB[$var]>=$max){
+                $max = $countB[$var];
+                $indB = $var;
+            }
+        }
+
+
+
+                // Finding Max Rep of array c
+        $countC = array(0,0,0,0);
+        for($var=0 ; $var<6; $var++){
+            echo "  ",$c[$var];
+        }
+        for($var=0 ; $var<6; $var++){
+            $temp = $c[$var];
+            $countC[$temp] = $countC[$temp] + 1;
+        }
+        for($var=0 ; $var<4; $var++){
+            echo "  ",$countC[$var];
+        }
+        $max = $countC[0];
+        $indC = 0;  //The index of the element max repeted
+        for($var=1; $var<4; $var++){
+            if($countC[$var]>=$max){
+                $max = $countC[$var];
+                $indC = $var;
+            }
+        }
+        $dsce->m1 = $indA;
+        $dsce->m2 = $indB;
+        $dsce->m3 = $indC;
+
+
         $dsce->save();
-        
+
         return redirect('/viewpso')->with('success', 'Edited Successfully, select the subject to view');
     }
 
--- resources/views/pages/editpso2.blade.php
+++ resources/views/pages/editpso2.blade.php
@@ -18,56 +18,56 @@
             </tr>
             <tr>
                 <th>CO.1</th>
-                <td>{{Form::text('zero1', $subject->zero1 , ['class' => 'form-control'])}}</td>
-                <td>{{Form::text('zero2', $subject->zero2 , ['class' => 'form-control'])}}</td>
-                <td>{{Form::text('zero3', $subject->zero3 , ['class' => 'form-control'])}}</td>
-               
+                <td>{{Form::text('zero1', $subject->zero1 , ['class' => 'form-control', 'required' => 'required'])}}</td>
+                <td>{{Form::text('zero2', $subject->zero2 , ['class' => 'form-control', 'required' => 'required'])}}</td>
+                <td>{{Form::text('zero3', $subject->zero3 , ['class' => 'form-control', 'required' => 'required'])}}</td>
+
             </tr>
             <tr>
                 <th>CO.2</th>
-                <td>{{Form::text('one1', $subject->one1, ['class' => 'form-control'])}}</td>
-                <td>{{Form::text('one2', $subject->one2, ['class' => 'form-control'])}}</td>
-                <td>{{Form::text('one3', $subject->one3, ['class' => 'form-control'])}}</td>
+                <td>{{Form::text('one1', $subject->one1, ['class' => 'form-control', 'required' => 'required'])}}</td>
+                <td>{{Form::text('one2', $subject->one2, ['class' => 'form-control', 'required' => 'required'])}}</td>
+                <td>{{Form::text('one3', $subject->one3, ['class' => 'form-control', 'required' => 'required'])}}</td>
                           </tr>
             <tr>
                 <th>CO.3</th>
-                <td>{{Form::text('two1', $subject->two1, ['class' => 'form-control'])}}</td>
-                <td>{{Form::text('two2', $subject->two2, ['class' => 'form-control'])}}</td>
-                <td>{{Form::text('two3', $subject->two3, ['class' => 'form-control'])}}</td>
+                <td>{{Form::text('two1', $subject->two1, ['class' => 'form-control', 'required' => 'required'])}}</td>
+                <td>{{Form::text('two2', $subject->two2, ['class' => 'form-control', 'required' => 'required'])}}</td>
+                <td>{{Form::text('two3', $subject->two3, ['class' => 'form-control', 'required' => 'required'])}}</td>
                            </tr>
             <tr>
                 <th>CO.4</th>
-                <td>{{Form::text('three1', $subject->three1, ['class' => 'form-control'])}}</td>
-                <td>{{Form::text('three2', $subject->three2, ['class' => 'form-control'])}}</td>
-                <td>{{Form::text('three3', $subject->three3, ['class' => 'form-control'])}}</td>
-                
+                <td>{{Form::text('three1', $subject->three1, ['class' => 'form-control', 'required' => 'required'])}}</td>
+                <td>{{Form::text('three2', $subject->three2, ['class' => 'form-control', 'required' => 'required'])}}</td>
+                <td>{{Form::text('three3', $subject->three3, ['class' => 'form-control', 'required' => 'required'])}}</td>
+
             </tr>
             <tr>
                 <th>CO.5</th>
-                <td>{{Form::text('four1', $subject->four1, ['class' => 'form-control'])}}</td>
-                <td>{{Form::text('four2', $subject->four2, ['class' => 'form-control'])}}</td>
-                <td>{{Form::text('four3', $subject->four3, ['class' => 'form-control'])}}</td>
-                
+                <td>{{Form::text('four1', $subject->four1, ['class' => 'form-control', 'required' => 'required'])}}</td>
+                <td>{{Form::text('four2', $subject->four2, ['class' => 'form-control', 'required' => 'required'])}}</td>
+                <td>{{Form::text('four3', $subject->four3, ['class' => 'form-control', 'required' => 'required'])}}</td>
+
             </tr>
             <tr>
                 <th>CO.6</th>
-                <td>{{Form::text('five1', $subject->five1, ['class' => 'form-control'])}}</td>
-                <td>{{Form::text('five2', $subject->five2, ['class' => 'form-control'])}}</td>
-                <td>{{Form::text('five3', $subject->five3, ['class' => 'form-control'])}}</td>
-                
+                <td>{{Form::text('five1', $subject->five1, ['class' => 'form-control', 'required' => 'required'])}}</td>
+                <td>{{Form::text('five2', $subject->five2, ['class' => 'form-control', 'required' => 'required'])}}</td>
+                <td>{{Form::text('five3', $subject->five3, ['class' => 'form-control', 'required' => 'required'])}}</td>
+
             </tr>
             <tr>
                 <th>Modal*</th>
-                <td>{{Form::text('m1', $subject->m1, ['class' => 'form-control'])}}</td>
-                <td>{{Form::text('m2', $subject->m2, ['class' => 'form-control'])}}</td>
-                <td>{{Form::text('m3', $subject->m3, ['class' => 'form-control'])}}</td>
-                
+                <td>{{Form::text('m1', $subject->m1, ['class' => 'form-control', 'readonly'])}}</td>
+                <td>{{Form::text('m2', $subject->m2, ['class' => 'form-control', 'readonly'])}}</td>
+                <td>{{Form::text('m3', $subject->m3, ['class' => 'form-control', 'readonly'])}}</td>
+
             </tr>
         </table>
         <small>Modal is the maximum frequency of occurence in a particular column.</small>
         {{Form::hidden('_method', 'PUT')}}
         {{Form::submit('Submit & Save', ['class' => 'btn btn-success'])}}
-        {!! Form::close() !!} 
-    @endforeach 
+        {!! Form::close() !!}
+    @endforeach
 </div>
 @endsection
